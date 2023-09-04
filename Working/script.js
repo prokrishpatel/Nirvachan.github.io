4856a5b6c2b967c00148756f645cc57c4f4b8ee8
“prokrishpatel” <pro.krishpatel@gmail.com>
2023-09-04 14:20:47 +0530

    login completed dashboard started

--- Working/script.js
+++ Working/script.js
@@ -20,3 +20,49 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
   }
   document.getElementById("default-login").click();
+
+  let slideIndex = 0;
+
+  function showSlides() {
+      let slides = document.querySelectorAll(".slide");
+      
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+
+      slideIndex++;
+
+      if (slideIndex > slides.length) {
+          slideIndex = 1;
+      }
+
+      slides[slideIndex - 1].style.display = "block";
+
+      // Change slides every 5 seconds
+      setTimeout(showSlides, 7000);
+  }
+
+  // Call the function to start the slideshow
+  showSlides();
+
+// Login Check
+
+document.getElementById("Login-form").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the form from submitting
+
+  // Get the user-entered values
+  var enteredUser = document.getElementById("luser").value;
+  var enteredPassword = document.getElementById("lpass").value;
+
+  // Check if the entered values match the expected values
+  if (enteredUser === "9302310053" && enteredPassword === "121023") {
+    // Redirect to dashboard.html
+    window.location.href = "dashboard.html";
+    return true
+  } else {
+      alert("Invalid credentials"); // Login failed
+      return false;
+  }
+});
+
+
